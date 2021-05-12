3db344acb91d9b1e0056e1fd371254560fb4f22c
Hector De Luna Luevano <delunaluevanohector030@gmail.com>
2021-05-11 22:23:10 -0500

    update

--- components/inicio.js
+++ components/inicio.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Container, Header, Title, Content, Footer, Button, FooterTab, Left, Right, Body, Icon, Text } from 'native-base';
 import { getTheme, StyleProvider, View, TouchableOpacity, Image } from 'native-base'
 import customVariables from '../theme/variables'
+import { Audio } from 'expo-av';
+
 
 export default function inicio(){
     const [sound, setSound] = React.useState();
