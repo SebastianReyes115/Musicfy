fc554c6034d624ab0f6ac18e161fed6282f82d6d
Hector De Luna Luevano <delunaluevanohector030@gmail.com>
2021-05-11 22:20:59 -0500

    reproducir

--- components/inicio.js
+++ components/inicio.js
@@ -1,18 +1,32 @@
 import React, { useState } from 'react';
 import { Container, Header, Title, Content, Footer, Button, FooterTab, Left, Right, Body, Icon, Text } from 'native-base';
-import { getTheme, StyleProvider } from 'native-base'
+import { getTheme, StyleProvider, View, TouchableOpacity, Image } from 'native-base'
 import customVariables from '../theme/variables'
 
 export default function inicio(){
-return(
-    <StyleProvider style={getTheme(customVariables)}>
-         <Container>
-        <Content>
-            <Text>
-                ESTAS EN INICIO
-            </Text>
-        </Content>
-    </Container>
-    </StyleProvider>
-);
+    const [sound, setSound] = React.useState();
+
+  async function playSound() {
+    console.log('Loading Sound');
+    const { sound } = await Audio.Sound.createAsync(
+       require('./assets/like.mp3')
+    );
+    setSound(sound);
+
+    console.log('Playing Sound');
+    await sound.playAsync(); }
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync(); }
+      : undefined;
+  }, [sound]);
+
+  return (
+    <View style={styles.container}>
+      <Button title="Play Sound" onPress={playSound} />
+    </View>
+  );
 }
